2f175fa0dcb6eecde8119438cfbf6f1e72217662
annro <42659227+annro-git@users.noreply.github.com>
2024-08-08 10:32:11 +0200

    add lato google font

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import { useFonts, Lato_300Light, Lato_400Regular, Lato_700Bold, Lato_900Black } from '@expo-google-fonts/lato';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let [loaded] = useFonts({
+    Lato_300Light, 
+    Lato_400Regular, 
+    Lato_700Bold, 
+    Lato_900Black,
+  })
+
+  if(!loaded){
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>UrbanBloom</Text>
       <StatusBar style="auto" />
     </View>
   );
