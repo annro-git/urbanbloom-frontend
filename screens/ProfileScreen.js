98903857d2b89361aacababa04dbfc17f8608864
DavidB <148340971+DavidB336@users.noreply.github.com>
2024-08-23 13:28:20 +0200

    display user gardens dans ProfilScreen OK

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -5,7 +5,7 @@ import * as LucideIcons from 'lucide-react-native';
 import { useDispatch } from "react-redux";
 import { updateUser } from "../reducers/user";
 import * as ImagePicker from 'expo-image-picker';
-
+import { Button, Modal } from 'antd';
 
 
 const ppURIToDispatch = ''
@@ -19,8 +19,10 @@ const ProfileScreen = props => {
     const { token, username, ppURI, } = useSelector(state => state.user);
     const user = useSelector(state => state.user)
     const [image, setImage] = useState(null);
+    const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
+    const [openModal, setOpenModal] = useState(false)
 
-    console.log(gardens)
+
 
     useEffect(() => {
         fetch(`${global.BACKEND_URL}/user/gardens`, {
@@ -34,16 +36,19 @@ const ProfileScreen = props => {
             .then(response => response.json())
             .then(data => {
 
-                console.log(data)
                 setGardens(data.gardens)
             })
     }
-        , [])
+        , [gardens])
 
     const pickImage = async () => {
 
+        if (status !== 'granted') {
+            requestPermission();
+        }
+
         // No permissions request is necessary for launching the image library
-        console.log('pickImage')
+
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
@@ -51,13 +56,36 @@ const ProfileScreen = props => {
             quality: 1,
         });
 
-        console.log("result", result);
-
         if (!result.canceled) {
             setImage(result.assets[0].uri)
             dispatch(updateUser({ ppURI: result.assets[0].uri }))
         }
     };
+
+    const createGarden = () => {
+
+        console.log('create garden')
+
+        fetch(`${global.BACKEND_URL}/garden`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token,
+            },
+            body: JSON.stringify({
+                name: 'new garden',
+                description: 'description',
+            })
+        }
+        )
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (data.success) {
+                    setGardens([...gardens, data.garden])
+                }
+            })
+        };
 
     return (
         <>
@@ -71,8 +99,8 @@ const ProfileScreen = props => {
                         {ppURI && <Image source={{ uri: ppURI }} style={styles.ppuri} />}
                     </TouchableOpacity>
                     <View style={styles.descriptionc}>
-                        <View style={styles.username}>
-                            <Text>{username}</Text>
+                        <View style={styles.usernamec}>
+                            <Text style={styles.username}>{username}</Text>
                             <LucideIcons.Pencil size={20} color={"black"} />
                         </View>
                         <Text style={styles.description} >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum,
@@ -81,17 +109,24 @@ const ProfileScreen = props => {
                         </Text>
                     </View>
                 </View>
-                <View style={styles.gardensc}>
-                    <Text>Jardins</Text>
-                    {gardens.map(garden => <View key={garden._id} title={garden.name} >
-                        <LucideIcons.Leaf></LucideIcons.Leaf>
-                        <Text>{garden.name}</Text>
-                        <LucideIcons.CircleX></LucideIcons.CircleX>
-                    </View>)}
+                <View style={styles.jardinsc}>
+                    <View style={styles.jardins}>
+                        <Text style={styles.jardin}>Jardins</Text>
+                        <TouchableOpacity onPress={()=>{setOpenModal(true)}} >
+                            <LucideIcons.CirclePlus style={styles.circleplus} size={20} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.gardensgrid}>
+                        {gardens.map(garden => <View key={garden._id} title={garden.name} style={styles.gardenc} >
+                            <LucideIcons.Leaf style={styles.leaf} />
+                            <Text> {garden.name}</Text>
+                        </View>)}
+                    </View>
                 </View>
                 <View style={styles.activitiesc}>
-                    <Text>Activité récente</Text>
+                    <Text style={styles.activites}>Activité récente</Text>
                 </View>
+                {openModal && <Modal style={styles.modal}></Modal>}
             </View>
 
         </>
@@ -110,41 +145,88 @@ const styles = StyleSheet.create({
         marginTop: 20,
         marginBottom: 20,
     },
-    gardensc: {
-
-    },
-    activitiesc: {
-
-    },
-    touchablepp: {
-        position: 'relative',
-        borderColor: 'black',
-        borderWidth: 1,
-        justifyContent: 'center',
-        marginLeft: 20,
-    },
     descriptionc: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
     description: {
-        borderColor: 'black',
-        borderWidth: 1,
-        marginTop: 20,
+        marginTop: 10,
         width: '90%',
     },
-    username: {
+    usernamec: {
         width: '90%',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        borderColor: 'black',
-        borderWidth: 1,
+    },
+    username: {
+        fontSize: 20,
     },
     ppuri: {
-        width: 50,
-        height: 50,
+        width: 70,
+        height: 70,
         borderRadius: 50,
     },
+    touchablepp: {
+        position: 'relative',
+        justifyContent: 'center',
+        marginLeft: 20,
+    },
+    jardinsc: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    jardins: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    jardin: {
+        fontSize: 20,
+        marginLeft: 20,
+    },
+    circleplus: {
+        color: 'green',
+        marginRight: 20,
+
+    },
+    gardensgrid: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        width: '90%',
+        gridTemplateColumns: 'repeat(2, 1fr)',
+
+    },
+    gardenc: {
+        flexDirection: 'row',
+        width: '50%',
+
+        height: 45,
+    },
+    leaf: {
+        color: 'green',
+        marginRight: 10,
+    },
+    circlex: {
+        color: 'red',
+        marginLeft: 10,
+    },
+
+
+    activitiesc: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    activites: {
+        fontSize: 20,
+        marginBottom: 20,
+        marginLeft: 20,
+    },
+
+
+
 })
