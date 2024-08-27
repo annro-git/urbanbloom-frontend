2671c092036d102cf384554bca577b2b0f7047c7
DavidB <148340971+DavidB336@users.noreply.github.com>
2024-08-27 09:53:34 +0200

    Update ForgotScreen.js

--- screens/ForgotScreen.js
+++ screens/ForgotScreen.js
@@ -10,7 +10,8 @@ import { useFonts, Lato_300Light, Lato_400Regular, Lato_700Bold, Lato_900Black }
             Lato_700Bold,
             Lato_900Black,
         })
-
+            
+               
         if (!loaded) {
             return null
         }
