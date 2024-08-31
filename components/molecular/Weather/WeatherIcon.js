f2b7238142b1305568f64bce4da18eeedf8345d6
annro <42659227+annro-git@users.noreply.github.com>
2024-08-31 10:36:21 +0200

    update weatherIcon

--- components/molecular/Weather/WeatherIcon.js
+++ components/molecular/Weather/WeatherIcon.js
@@ -3,36 +3,36 @@ import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { Sun, CloudSun, CloudFog, CloudSunRain, CloudRain, CloudRainWind, CloudSnow, CloudLightning, Snowflake, CloudDrizzle, CloudHail } from "lucide-react-native"
 
-const wmoValues = [
-    {wmo: 0, icon: Sun, text: 'Dégagé'},
-    {wmo: 1, icon: Sun, text: 'Parsemé'},
-    {wmo: 2, icon: CloudSun, text: 'Nuageux'},
-    {wmo: 3, icon: CloudSun, text: 'Couvert'},
-    {wmo: 45, icon: CloudFog, text: 'Brouillard'},
-    {wmo: 48, icon: CloudFog, text: 'Brumeux'},
-    {wmo: 51, icon: CloudFog, text: 'Bruine'},
-    {wmo: 53, icon: CloudDrizzle, text: 'Bruine'},
-    {wmo: 55, icon: CloudDrizzle, text: 'Bruine'},
-    {wmo: 56, icon: CloudDrizzle, text: 'Bruine verglaçante'},
-    {wmo: 57, icon: CloudDrizzle, text: 'Bruine verglaçante'},
-    {wmo: 61, icon: CloudSunRain, text: 'Pluie'},
-    {wmo: 63, icon: CloudSunRain, text: 'Pluie'},
-    {wmo: 65, icon: CloudRain, text: 'Pluie'},
-    {wmo: 66, icon: CloudRainWind, text: 'Pluie verglaçante'},
-    {wmo: 67, icon: CloudRainWind, text: 'Pluie verglaçante'},
-    {wmo: 71, icon: CloudSnow, text: 'Neige'},
-    {wmo: 73, icon: CloudSnow, text: 'Neige'},
-    {wmo: 75, icon: Snowflake, text: 'Neige'},
-    {wmo: 77, icon: Snowflake, text: 'Neige'},
-    {wmo: 80, icon: CloudRainWind, text: 'Averses'},
-    {wmo: 81, icon: CloudRainWind, text: 'Averses'},
-    {wmo: 82, icon: CloudRainWind, text: 'Averses'},
-    {wmo: 85, icon: CloudSnow, text: 'Neige'},
-    {wmo: 86, icon: CloudSnow, text: 'Neige'},
-    {wmo: 95, icon: CloudLightning, text: 'Orageux'},
-    {wmo: 96, icon: CloudHail, text: 'Grêle'},
-    {wmo: 99, icon: CloudLightning, text: 'Orage et grêle'},
-]
+const wmoValues = {
+    0: {icon: Sun, text: 'Dégagé'},
+    1: {icon: Sun, text: 'Parsemé'},
+    2: {icon: CloudSun, text: 'Nuageux'},
+    3: {icon: CloudSun, text: 'Couvert'},
+    45: {icon: CloudFog, text: 'Brouillard'},
+    48: {icon: CloudFog, text: 'Brumeux'},
+    51: {icon: CloudFog, text: 'Bruine'},
+    53: {icon: CloudDrizzle, text: 'Bruine'},
+    55: {icon: CloudDrizzle, text: 'Bruine'},
+    56: {icon: CloudDrizzle, text: 'Bruine verglaçante'},
+    57: {icon: CloudDrizzle, text: 'Bruine verglaçante'},
+    61: {icon: CloudSunRain, text: 'Pluie'},
+    63: {icon: CloudSunRain, text: 'Pluie'},
+    65: {icon: CloudRain, text: 'Pluie'},
+    66: {icon: CloudRainWind, text: 'Pluie verglaçante'},
+    67: {icon: CloudRainWind, text: 'Pluie verglaçante'},
+    71: {icon: CloudSnow, text: 'Neige'},
+    73: {icon: CloudSnow, text: 'Neige'},
+    75: {icon: Snowflake, text: 'Neige'},
+    77: {icon: Snowflake, text: 'Neige'},
+    80: {icon: CloudRainWind, text: 'Averses'},
+    81: {icon: CloudRainWind, text: 'Averses'},
+    82: {icon: CloudRainWind, text: 'Averses'},
+    85: {icon: CloudSnow, text: 'Neige'},
+    86: {icon: CloudSnow, text: 'Neige'},
+    95: {icon: CloudLightning, text: 'Orageux'},
+    96: {icon: CloudHail, text: 'Grêle'},
+    99: {icon: CloudLightning, text: 'Orage et grêle'},
+}
 
 const WeatherIcon = ({ color }) => {
 
@@ -57,10 +57,12 @@ const WeatherIcon = ({ color }) => {
         }
     }, [user])
 
+    console.log(weatherIcon)
+
     return (
         <View style={{ flexDirection: 'row', alignItems: 'flex-end', }}>
             {weatherIcon.wmo &&
-                wmoValues.find(e => e.wmo === weatherIcon.wmo).icon.render({color, size: 32})
+                wmoValues[weatherIcon.wmo].icon.render({color, size: 32})
             }
             { weatherIcon.temp &&
                 <Text style={{ color: color, fontSize: 16, fontFamily: 'Lato_700Bold' }}>{Math.round(weatherIcon.temp)}°C</Text>
